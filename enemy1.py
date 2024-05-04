3b694f8db1fcafdb54b2db50063f4f2efc3bb6c8
genesictidi <151385871+genesictidi@users.noreply.github.com>
2024-05-04 19:03:33 +0300

    j;nkn,nm,m

--- enemy1.py
+++ enemy1.py
@@ -5,18 +5,26 @@ class Enemy1:
     def __init__(self, screen):
         self.screen = screen
         self.x = 700
-        self.y = 130
+        self.y = 100
         self.target_x = 25
         self.target_y = 200
         self.player = Player(self.screen)
-        self.enemy_rect = None
+        
 
     def draw(self):
-        self.enemy_rect = pg.draw.circle(self.screen, 'red', (self.x, self.y), 25)
+        #self.enemy_rect = pg.draw.circle(self.screen, 'red', (self.x, self.y), 25)
+        enemy1_surf = pg.image.load('sources/cactus.png').convert_alpha()
+
+        scale = pg.transform.scale(
+                enemy1_surf, (enemy1_surf.get_width() // 4,
+                    enemy1_surf.get_height() // 4))
+        scale_rect = scale.get_rect(center=(self.x, 100))
+        self.me = scale_rect
+        self.screen.blit(scale, scale_rect)
 
     def move(self):                         
         if self.x > self.target_x:
-            self.x -= 10
+            self.x -= 9
         else:
             self.x += 700
         
--- player.py
+++ player.py
@@ -3,19 +3,19 @@ import pygame as pg
 class Player:
     def __init__(self, screen):
         self.screen = screen
-        self.x = 350
-        self.y = 700
-        self.me = None
+        self.x = 300
+        self.y = 100
         self.jump = True
         self.jump_size = 7
         
     def draw(self):
-        player_surf = pg.image.load('sources/dino.png').convert_alpha()
+        player_surf = pg.image.load('sources/dino2.png').convert_alpha()
 
         scale = pg.transform.scale(
-                player_surf, (player_surf.get_width() // 6,
-                    player_surf.get_height() // 6))
-        scale_rect = scale.get_rect(center=(350, 130))
+                player_surf, (player_surf.get_width() // 4,
+                    player_surf.get_height() // 4))
+        scale_rect = scale.get_rect(center=(300, self.y))
+        self.me = scale_rect
         self.screen.blit(scale, scale_rect)
 
     def move(self, keys):
